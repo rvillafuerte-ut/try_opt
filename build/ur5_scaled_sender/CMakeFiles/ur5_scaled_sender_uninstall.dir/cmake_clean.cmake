cf235b09d9d5e20ba99d5a4c3ebac0c01bfd92a5
Rodrigo V <rvillafuerte@utec.edu.pe>
2025-12-01 18:02:43 -0500

    Stop tracking build artifacts (build/, install/, log/)

--- build/ur5_scaled_sender/CMakeFiles/ur5_scaled_sender_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ur5_scaled_sender_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ur5_scaled_sender_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/ur5_scaled_sender/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/utec/try_opt/src/ur5_scaled_sender
-# Build directory: /home/utec/try_opt/build/ur5_scaled_sender
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
